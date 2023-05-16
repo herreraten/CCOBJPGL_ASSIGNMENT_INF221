1ca3d6e7a422a5bf870d4533eb39c6a49060a332
herreraten <112837135+herreraten@users.noreply.github.com>
2023-05-16 11:32:18 +0800

    Create App.java

---
+++ MyDog/App.java
@@ -0,0 +1,12 @@
+package MyDog;
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // Create object from pet class
+        poodle myPet = new poodle();
+
+        myPet.bark();
+        System.out.println(myPet.mouth);
+        System.out.println(myPet.bark);
+    }
+}
